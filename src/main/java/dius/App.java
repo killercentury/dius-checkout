d124e2336eb977d84fda9ced79dad38299d0c074
Kira Cheung <killercentury@gmail.com>
2015-08-02 15:32:35 +1000

    update App main

--- src/main/java/dius/App.java
+++ src/main/java/dius/App.java
@@ -1,7 +1,22 @@
 package dius;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Store store = new Store();
+        store.setName("DiUS Computer Store");
+        List<Product> productList = new ArrayList<>();
+        Product ipd = new Product("ipd", "Super iPad", new BigDecimal("549.99"));
+        Product mbp = new Product("mbp", "MacBook Pro", new BigDecimal("1399.99"));
+        Product atv = new Product("atv", "Apple TV", new BigDecimal("109.50"));
+        Product vga = new Product("vga", "VGA adapter", new BigDecimal("30.00"));
+        productList.add(ipd);
+        productList.add(mbp);
+        productList.add(atv);
+        productList.add(vga);
+        store.setProductList(productList);
     }
 }
